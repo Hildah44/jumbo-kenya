e7a6fc8bfe3b79e04040321ca2ced2c7dd36a163
Hildah44 <163909250+Hildah44@users.noreply.github.com>
2024-03-18 22:26:47 +0300

    Create jumbo kenya.java

---
+++ jumbo kenya.java
@@ -0,0 +1,9 @@
+public class DisplayJumboKenya {
+    public static void main(String[] args) {
+        System.out.println("  JJJJ  U   U  M   M  BBBBB   OOO ");
+        System.out.println("    J    U   U  MM MM  B    B O   O");
+        System.out.println("    J    U   U  M M M  BBBBB  O   O");
+        System.out.println("J   J    U   U  M   M  B    B O   O");
+        System.out.println(" JJJ      UUU   M   M  BBBBB   OOO ");
+    }
+}
